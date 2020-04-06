3ceaf6da8bfbe1e5d2f6492465fd90e58602a795
Rahul Babu K <rahulbabu101@gmail.com>
2020-04-06 14:11:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,7 @@ import string
 import re
 import pandas as pd
 import numpy as np
-from bs4 import BeautifulSoup as bs
-import requests
 
-
-base = "https://www.youtube.com/results?search_query="
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 df = pd.read_csv("mpr8.csv")
